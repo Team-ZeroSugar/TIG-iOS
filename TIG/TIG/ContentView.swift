247b88ca045a0c880ec421fca476e52259d698da
Jeong Dong <eejxdong99@gmail.com>
2024-07-09 10:34:05 +0900

    chore: 프로젝트 세팅

---
+++ TIG/TIG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TIG
+//
+//  Created by 이정동 on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Text("Hello")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TIG/TIG/TIGApp.swift
@@ -0,0 +1,18 @@
+//
+//  TIGApp.swift
+//  TIG
+//
+//  Created by 이정동 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TIGApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
