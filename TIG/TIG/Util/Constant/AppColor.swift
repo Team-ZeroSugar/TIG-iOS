584f5175febb2064468fabec8164839abcbf1def
Jeong Dong <eejxdong99@gmail.com>
2024-07-10 18:22:41 +0900

    style: Color Asset 정의

--- TIG/TIG/Util/Constant/AppColor.swift
+++ TIG/TIG/Util/Constant/AppColor.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct AppColor {
-    
+    static let gray0 = Color(.gray0)
+    static let gray1 = Color(.gray1)
+    static let gray2 = Color(.gray2)
+    static let gray3 = Color(.gray3)
+    static let gray4 = Color(.gray4)
+    static let gray5 = Color(.gray5)
+    static let white = Color(.mainWhite)
+    static let mainBlue = Color(.mainBlue)
+    static let timelineBlue = Color(.timelineBlue)
+    static let timelineStroke = Color(.timelineStroke)
+    static let bgLight = Color(.bgLight)
+}
+
+extension LinearGradient {
+    static let main = Self(
+        colors: [
+            .linearGradientTop,
+            .linearGradientMid,
+            .linearGradientBot
+        ],
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
 }
