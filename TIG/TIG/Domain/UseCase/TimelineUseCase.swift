bc4993cbc16d3194b921a2f978bb32ec89909d16
shinseungjae <mok05247@naver.com>
2024-07-21 16:37:53 +0900

    feat: TimelineUseCase에 editTimeline, timeString 추가

--- TIG/TIG/Domain/UseCase/TimelineUseCase.swift
+++ TIG/TIG/Domain/UseCase/TimelineUseCase.swift
@@ -13,11 +13,35 @@ class TimelineUseCase {
     // MARK: - State
     struct State {
         // View State
+        var editTimeline: Bool = false
         
         // Data State
+        var timelines: [Timeline] = []
     }
     
     // MARK: - Action
+    enum Action {
+        case tappedEditTimeline
+    }
+    
+    private var _state: State = .init()
+    var state: State {
+        return _state
+    }
     
     // MARK: - View Action
+    func effect(_ action: Action) {
+        switch action {
+        case .tappedEditTimeline:
+            _state.editTimeline.toggle()
+        }
+    }
+    
+    // MARK: - Helper function
+    func timeString(for index: Int) -> String {
+        let hour = index / 2 % 24
+        let period = hour < 12 ? "오전" : "오후"
+        let displayHour = hour % 12 == 0 ? 12 : hour % 12
+        return "\(period) \(displayHour)시"
+    }
 }
