9c29b1139d736816d9bbdc46f19fa2f05ce2b754
shinseungjae <mok05247@naver.com>
2024-07-21 15:23:05 +0900

    refactor: 설정 RoundedRectangle 그리는 방식 변경

--- TIG/TIG/Presentaion/TimeLineView.swift
+++ TIG/TIG/Presentaion/TimeLineView.swift
@@ -9,18 +9,18 @@ import SwiftUI
 
 struct TimelineView: View {
     
-    @State var isEditing: Bool = false
+    @State var isEditMode: Bool = false
     
     var body: some View {
         ScrollView {
             VStack {
                 Spacer().frame(height: 47)
                 
-                TimelineHeaderView(isEditing: $isEditing)
+                TimelineHeaderView(isEditMode: $isEditMode)
                 
                 Spacer().frame(height: 51)
                 
-                TimelineBodyView(isEditing: $isEditing)
+                TimelineBodyView(isEditMode: $isEditMode)
             }
             .padding(.horizontal, 20)
         }
@@ -29,20 +29,20 @@ struct TimelineView: View {
 
 // MARK: - Header View
 fileprivate struct TimelineHeaderView: View {
-    @Binding var isEditing: Bool
+    @Binding var isEditMode: Bool
     
     fileprivate var body: some View {
         HStack {
-            Text(isEditing ? "오늘 일정 시간을 탭해서 지워주세요" : "지금은 활용 가능 시간이에요")
+            Text(isEditMode ? "오늘 일정 시간을 탭해서 지워주세요" : "지금은 활용 가능 시간이에요")
                 .font(.custom(AppFont.semiBold, size: 18))
                 .foregroundStyle(AppColor.gray5)
             
             Spacer()
             
             Button(action: {
-                isEditing.toggle()
+                isEditMode.toggle()
             }, label: {
-                Text(isEditing ? "완료" : "편집")
+                Text(isEditMode ? "완료" : "편집")
                     .font(.custom(AppFont.medium, size: 16))
                     .foregroundStyle(AppColor.mainBlue)
             })
@@ -53,47 +53,52 @@ fileprivate struct TimelineHeaderView: View {
 // MARK: - Body View
 fileprivate struct TimelineBodyView: View {
     
-    @Binding var isEditing: Bool
+    @Binding var isEditMode: Bool
     
     fileprivate var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            ForEach(18..<54) { index in
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack(alignment: .top, spacing: 0) {
-                        Text(timeString(for: index))
-                            .frame(width: 47, height: 14, alignment: .leading)
-                            .font(.custom(AppFont.medium, size: 12))
-                            .foregroundStyle(AppColor.gray3)
-                            .opacity(index % 2 == 0 ? 1 : 0)
-                            .offset(y: -7)
-                        
-                        Spacer().frame(width: 14)
-                        
-                        Rectangle()
-                            .frame(width: index % 2 == 0 ? 28 : 16, height: 1)
-                            .foregroundStyle(AppColor.gray2)
+        HStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(18..<54) { index in
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack(alignment: .top, spacing: 0) {
+                            Text(timeString(for: index))
+                                .frame(width: 47, height: 14, alignment: .leading)
+                                .font(.custom(AppFont.medium, size: 12))
+                                .foregroundStyle(AppColor.gray3)
+                                .opacity(index % 2 == 0 ? 1 : 0)
+                                .offset(y: -7)
                             
-                        Spacer().frame(width: index % 2 == 0 ? 7 : 19)
-                        
-                        if isEditing {
-                            VStack {
-                                Spacer().frame(height: 2)
-                                
-                                RoundedRectangle(cornerRadius: 8)
-                                    .frame(height: 35)
-                                    .foregroundStyle(AppColor.timelineBlue)
-                                
-                                Spacer().frame(height: 2)
-                            }
-                        } else {
+                            Spacer().frame(width: 14, height: 39)
+                            
                             Rectangle()
-                                .frame(width: 4, height: 39)
-                                .foregroundStyle(AppColor.timelineBlue)
-                            
-                            Spacer()
+                                .frame(width: index % 2 == 0 ? 28 : 16, height: 1)
+                                .foregroundStyle(AppColor.gray2)
                         }
                     }
                 }
+            }
+            
+            Spacer().frame(width: 18)
+            
+            if isEditMode {
+                VStack(alignment: .leading, spacing:4) {
+                    ForEach(18..<54) { index in
+                        Button(action: {
+                            
+                        }, label: {
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundStyle(.timelineBlue)
+                                .frame(height: 35)
+                        })
+                    }
+                }
+            } else {
+                VStack(alignment: .leading, spacing:0) {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundStyle(.timelineBlue)
+                        .frame(width: 4)
+                }
+                Spacer()
             }
         }
     }
