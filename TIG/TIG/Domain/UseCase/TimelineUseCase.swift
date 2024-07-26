c22dc6f7e8fc2291e2a3148245940ccf7cdf8cc9
shinseungjae <mok05247@naver.com>
2024-07-26 20:09:44 +0900

    feat: TimelineUseCase에서 appSettingService, dailyDataSevice 받을 수 있게 변경

--- TIG/TIG/Domain/UseCase/TimelineUseCase.swift
+++ TIG/TIG/Domain/UseCase/TimelineUseCase.swift
@@ -16,114 +16,16 @@ class TimelineUseCase {
         var editTimeline: Bool = false
         
         // Data State
-        // 구현을 위한 임시 Data
-        //var timelines: [Timeline] = []
-        var timelines: [Timeline] = [
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 09:00 - 09:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 09:30 - 10:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 10:00 - 10:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 10:30 - 11:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 11:00 - 11:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 11:30 - 12:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 12:00 - 12:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 12:30 - 13:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 13:00 - 13:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 13:30 - 14:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 14:00 - 14:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 14:30 - 15:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 15:00 - 15:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 15:30 - 16:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 16:00 - 16:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 16:30 - 17:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 17:00 - 17:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 17:30 - 18:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 30))!,
-                     isAvailable: false),  // 2024/07/21 18:00 - 18:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 18:30 - 19:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 30))!,
-                     isAvailable: false),  // 2024/07/21 19:00 - 19:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 19:30 - 20:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 30))!,
-                     isAvailable: false),  // 2024/07/21 20:00 - 20:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 20:30 - 21:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 30))!,
-                     isAvailable: false),  // 2024/07/21 21:00 - 21:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 21:30 - 22:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 30))!,
-                     isAvailable: false),  // 2024/07/21 22:00 - 22:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 0))!,
-                     isAvailable: false), // 2024/07/21 22:30 - 23:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 30))!,
-                     isAvailable: true),  // 2024/07/21 23:00 - 23:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 0))!,
-                     isAvailable: true), // 2024/07/21 23:30 - 00:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 30))!,
-                     isAvailable: true),  // 2024/07/22 00:00 - 00:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 0))!,
-                     isAvailable: true), // 2024/07/22 00:30 - 01:00
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 0))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 30))!,
-                     isAvailable: true),  // 2024/07/22 01:00 - 01:30
-            Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 30))!,
-                     end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 2, minute: 0))!,
-                     isAvailable: false)  // 2024/07/22 01:30 - 02:00
-        ]
-        let wakeupTime = Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))! // 7월 21일 오전 9시
-        let bedTime = Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 2, minute: 0))! // 7월 22일 오전 2시
+        var currentDate: Date = Date()
+        var isWeekly: Bool = false
+        var wakeupTime: Date = Date()
+        var bedTime: Date = Date()
+        var timelines: [Timeline] = []
+        
+        init(currentDate: Date, isWeekly: Bool) {
+            self.currentDate = currentDate
+            self.isWeekly = isWeekly
+        }
     }
     
     // MARK: - Action
@@ -131,7 +33,32 @@ class TimelineUseCase {
         case tappedEditTimeline
     }
     
-    private(set) var state: State = .init()
+    private var appSettingService: AppSettingRepository
+    private var dailyDataService: DailyContentRepository
+    //private var weeklyDataService: WeeklyRepeatRepository
+    private(set) var state: State
+    
+    init(appSettingService: AppSettingRepository,
+         dailyDataService: DailyContentRepository,
+         //weeklyDataService: WeeklyRepeatRepository,
+         currentDate: Date = Date(),
+         isWeelky: Bool = false
+    ) {
+        self.appSettingService = appSettingService
+        self.dailyDataService = dailyDataService
+        //self.weeklyDataService = weeklyDataService
+        self.state = State(currentDate: currentDate, isWeekly: isWeelky)
+        
+        let settings = appSettingService.getAppSettings()
+        state.wakeupTime = settings.wakeupTime
+        state.bedTime = settings.bedTime
+        
+        if !state.isWeekly {
+            updateTimelinesForCurrentDate()
+        } else {
+            //updateTimelinesForWeeklyReapts()
+        }
+    }
     
     // MARK: - View Action
     func effect(_ action: Action) {
@@ -190,4 +117,28 @@ class TimelineUseCase {
         return result
     }
 
+}
+
+extension TimelineUseCase {
+    private func updateTimelinesForCurrentDate() {
+        switch dailyDataService.fetchDailyContents() {
+        case .success(let dailyContents):
+            if let matchingContent = dailyContents.first(where: { Calendar.current.isDate($0.date, inSameDayAs: state.currentDate) }) {
+                state.timelines = matchingContent.timelines
+            } else {
+                state.timelines = []
+            }
+        case .failure:
+            return
+        }
+    }
+    
+//    private func updateTimelinesForWeeklyReapts() {
+//        switch weeklyDataService.fetchWeeklyRepeats() {
+//        case .success(let weeklyContents):
+//            print(weeklyContents)
+//        case .failure:
+//            return
+//        }
+//    }
 }
--- TIG/TIG/Presentaion/TimeLineView.swift
+++ TIG/TIG/Presentaion/TimeLineView.swift
@@ -218,5 +218,153 @@ fileprivate struct TimelineContentView: View {
 }
 
 #Preview {
-    TimelineView(timelineUseCase: TimelineUseCase())
+    TimelineView(timelineUseCase: TimelineUseCase(appSettingService: TestAppSettingsService(), dailyDataService: TestDailyDataService(), currentDate: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))!, isWeelky: false))
+}
+
+class TestAppSettingsService: AppSettingRepository {
+    func updateAppSettings(_ appSetting: AppSetting) {
+        print("hello")
+    }
+    
+    func getAppSettings() -> AppSetting {
+        
+        let wakeupTime = Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))! // 7월 21일 오전 9시
+        let bedTime = Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 2, minute: 0))! // 7월 22일 오전 2시
+        
+        return AppSetting(wakeupTime: wakeupTime, bedTime: bedTime, isLightMode: true, allowNotifications: true)
+    }
+    
+    
+}
+
+class TestDailyDataService: DailyContentRepository {
+    func createDailyContent(_ dailyContent: DailyContent) {
+        print("hello")
+    }
+    
+    func fetchDailyContents() -> Result<[DailyContent], SwiftDataError> {
+        
+        let timelines: [Timeline] = [
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 09:00 - 09:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 09:30 - 10:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 10:00 - 10:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 10, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 10:30 - 11:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 11:00 - 11:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 11, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 11:30 - 12:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 12:00 - 12:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 12, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 12:30 - 13:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 13:00 - 13:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 13, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 13:30 - 14:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 14:00 - 14:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 14, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 14:30 - 15:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 15:00 - 15:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 15, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 15:30 - 16:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 16:00 - 16:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 16, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 16:30 - 17:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 17:00 - 17:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 17, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 17:30 - 18:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 30))!,
+                             isAvailable: false),  // 2024/07/21 18:00 - 18:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 18, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 18:30 - 19:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 30))!,
+                             isAvailable: false),  // 2024/07/21 19:00 - 19:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 19, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 19:30 - 20:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 30))!,
+                             isAvailable: false),  // 2024/07/21 20:00 - 20:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 20, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 20:30 - 21:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 30))!,
+                             isAvailable: false),  // 2024/07/21 21:00 - 21:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 21, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 21:30 - 22:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 30))!,
+                             isAvailable: false),  // 2024/07/21 22:00 - 22:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 22, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 0))!,
+                             isAvailable: false), // 2024/07/21 22:30 - 23:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 30))!,
+                             isAvailable: true),  // 2024/07/21 23:00 - 23:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 23, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 0))!,
+                             isAvailable: true), // 2024/07/21 23:30 - 00:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 30))!,
+                             isAvailable: true),  // 2024/07/22 00:00 - 00:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 0, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 0))!,
+                             isAvailable: true), // 2024/07/22 00:30 - 01:00
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 0))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 30))!,
+                             isAvailable: true),  // 2024/07/22 01:00 - 01:30
+                    Timeline(start: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 1, minute: 30))!,
+                             end: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 22, hour: 2, minute: 0))!,
+                             isAvailable: false)  // 2024/07/22 01:30 - 02:00
+                ]
+        
+        let dailycontents = [
+            DailyContent(date: Calendar.current.date(from: DateComponents(year: 2024, month: 7, day: 21, hour: 9, minute: 0))!, timelines: timelines, totalAvailabilityTime: 8),
+        ]
+        
+        do {
+            return .success(dailycontents)
+        } catch {
+            return .failure(.fetchError)
+        }
+    }
+    
+    func updateDailyContent(dailyContent: DailyContent, timelines: [Timeline]) {
+        print("hello")
+    }
+    
+    func deleteDailyContent(_ dailyContent: DailyContent) {
+        print("hello")
+    }
 }
