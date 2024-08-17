4b47b3fdc89ab2dee1ab2e17b26add3d8939cd63
Seo-Jooyoung <tjwndud0412@naver.com>
2024-08-17 18:01:55 +0900

    feat: AnnounceView 레이아웃 수정

--- TIG/TIG/Feature/Home/TimerAnnounceView.swift
+++ TIG/TIG/Feature/Home/TimerAnnounceView.swift
@@ -15,15 +15,15 @@ struct TimerAnnounceView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 48)
-                Spacer().frame(height: 27)
+                Spacer().frame(height: 24)
                 Text("오늘 일정을 설정해 보세요")
                     .font(.custom(AppFont.semiBold, size: 20))
-                Spacer().frame(height: 13)
+                Spacer().frame(height: 12)
                 Text("자유롭게 활용 가능한 시간을 알려줄게요")
                     .font(.custom(AppFont.regular, size: 16))
                     .foregroundStyle(AppColor.gray04)
             }
-            Spacer().frame(height: 42)
+            Spacer().frame(height: 32)
             Button(action: {
                 
             }, label: {
