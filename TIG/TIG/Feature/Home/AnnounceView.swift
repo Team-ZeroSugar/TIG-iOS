1c384e4551886cda2e460618e0421e4f3d6ac5d1
Seo-Jooyoung <tjwndud0412@naver.com>
2024-08-23 15:05:51 +0900

    refactor: 설정 전 AnnounceView 레이아웃 및 폰트 크기 수정

--- TIG/TIG/Feature/Home/AnnounceView.swift
+++ TIG/TIG/Feature/Home/AnnounceView.swift
@@ -19,10 +19,11 @@ struct AnnounceView: View {
                     .frame(width: 48)
                 Spacer().frame(height: 24)
                 Text("오늘 일정을 설정해 보세요")
-                    .font(.custom(AppFont.semiBold, size: 20))
+                    .font(.custom(AppFont.semiBold, size: 18))
+                    .foregroundStyle(AppColor.gray05)
                 Spacer().frame(height: 12)
                 Text(homeViewModel.state.activeTab == .time ? "자유롭게 활용 가능한 시간을 알려줄게요" : "자유롭게 활용 가능한 타임라인을 알려줄게요")
-                    .font(.custom(AppFont.regular, size: 16))
+                    .font(.custom(AppFont.regular, size: 14))
                     .foregroundStyle(AppColor.gray04)
             }
             Spacer().frame(height: 32)
