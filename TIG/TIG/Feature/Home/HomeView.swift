a1395deedc5fc4c6a7cb0937c0b1d6fe1f6eb0ea
shinseungjae <mok05247@naver.com>
2024-09-12 21:20:06 +0900

    feat: HomeView에서 분기처리

--- TIG/TIG/Feature/Home/HomeView.swift
+++ TIG/TIG/Feature/Home/HomeView.swift
@@ -145,6 +145,9 @@ fileprivate struct ScrollableTabBar: View {
           selectedTabOffset = (proxy.size.width / 2) * CGFloat(Tab.allCases.firstIndex(of: new.unsafelyUnwrapped) ?? 0)
         }
       }
+      .onChange(of: homeViewModel.state.activeTab) { oldValue, newValue in
+          scrollPosition = newValue
+      }
     }
     
   }
@@ -194,10 +197,18 @@ fileprivate struct ScrollableTabBar: View {
           VStack {
             switch tab {
             case .time:
-              TimerView()
+                if homeViewModel.state.dailyContent.timelines.isEmpty {
+                    AnnounceView()
+                } else {
+                    TimerView()
+                }
               
             case .timeline:
-              TimelineView()
+                if homeViewModel.state.dailyContent.timelines.isEmpty {
+                    AnnounceView(isTimelineView: true)
+                } else {
+                    TimelineView()
+                }
             }
           }
           .frame(width: size.width)
