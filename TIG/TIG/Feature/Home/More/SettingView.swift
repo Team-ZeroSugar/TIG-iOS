e4360af227da0962e4a248b75494ee276bcf6068
Jeong Dong <eejxdong99@gmail.com>
2024-09-26 13:37:11 +0900

    feat: SettingView 수면시간 설정 화면 수정

--- TIG/TIG/Feature/Home/More/SettingView.swift
+++ TIG/TIG/Feature/Home/More/SettingView.swift
@@ -26,76 +26,74 @@ struct SettingView: View {
   var body: some View {
     List {
       Section("수면 시간 설정") {
+        HStack {
+          Text("취침 시간")
+            .font(.custom(AppFont.medium, size: 16))
+            .foregroundStyle(AppColor.gray04)
+          Spacer()
+          Button(action: {
+            isPresentBedTimePicker = true
+            bedPickerIndex = bedTimeIndex
+          }, label: {
+            Text("\(bedTimeIndex.convertToKoreanTimeFormat())")
+              .font(.custom(AppFont.medium, size: 16))
+              .foregroundStyle(AppColor.blueMain)
+              .padding(4)
+          })
+          .padding(6)
+          .background(AppColor.gray00)
+          .foregroundStyle(.white)
+          .clipShape(RoundedRectangle(cornerRadius: 5))
+          .buttonStyle(.plain)
+          .confirmationDialog(isPresented: $isPresentBedTimePicker) {
+            CustomWheelPicker(selectedIndex: $bedPickerIndex)
+              .background {
+                RoundedRectangle(cornerRadius: 10)
+                  .fill(AppColor.blueMain.opacity(0.5))
+                  .frame(width: 300, height: 40)
+              }
+          } actions: {
+            SheetAction(title: "확인", role: .default) {
+              isShownBedAlert = true
+            }
+            SheetAction(title: "취소", role: .cancel)
+          }
+        }
         
-          HStack {
-            Text("기상 시간")
+        HStack {
+          Text("기상 시간")
+            .font(.custom(AppFont.medium, size: 16))
+            .foregroundStyle(AppColor.gray04)
+          Spacer()
+          Button(action: {
+            isPresentWakeupTimePicker = true
+            wakeupPickerIndex = wakeupTimeIndex
+          }, label: {
+            Text("\(wakeupTimeIndex.convertToKoreanTimeFormat())")
               .font(.custom(AppFont.medium, size: 16))
-              .foregroundStyle(AppColor.gray04)
-            Spacer()
-            Button(action: {
-              isPresentWakeupTimePicker = true
-              wakeupPickerIndex = wakeupTimeIndex
-            }, label: {
-              Text("\(wakeupTimeIndex.convertToKoreanTimeFormat())")
-                .font(.custom(AppFont.medium, size: 16))
-                .foregroundStyle(AppColor.blueMain)
-                .padding(4)
-            })
-            .padding(6)
-            .background(AppColor.gray00)
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 5))
-            .buttonStyle(.plain)
-            .confirmationDialog(isPresented: $isPresentWakeupTimePicker) {
-              CustomWheelPicker(selectedIndex: $wakeupPickerIndex)
-                .background {
-                  RoundedRectangle(cornerRadius: 10)
-                    .fill(AppColor.blueMain.opacity(0.5))
-                    .frame(width: 300, height: 40)
-                }
-            } actions: {
-              SheetAction(title: "ok", role: .default) {
-                isShownWakeupAlert = true
+              .foregroundStyle(AppColor.blueMain)
+              .padding(4)
+          })
+          .padding(6)
+          .background(AppColor.gray00)
+          .foregroundStyle(.white)
+          .clipShape(RoundedRectangle(cornerRadius: 5))
+          .buttonStyle(.plain)
+          .confirmationDialog(isPresented: $isPresentWakeupTimePicker) {
+            CustomWheelPicker(selectedIndex: $wakeupPickerIndex)
+              .background {
+                RoundedRectangle(cornerRadius: 10)
+                  .fill(AppColor.blueMain.opacity(0.5))
+                  .frame(width: 300, height: 40)
               }
-              SheetAction(title: "cancel", role: .cancel)
+          } actions: {
+            SheetAction(title: "확인", role: .default) {
+              isShownWakeupAlert = true
             }
-
+            SheetAction(title: "취소", role: .cancel)
           }
           
-          
-          HStack {
-            Text("취침 시간")
-              .font(.custom(AppFont.medium, size: 16))
-              .foregroundStyle(AppColor.gray04)
-            Spacer()
-            Button(action: {
-              isPresentBedTimePicker = true
-              bedPickerIndex = bedTimeIndex
-            }, label: {
-              Text("\(bedTimeIndex.convertToKoreanTimeFormat())")
-                .font(.custom(AppFont.medium, size: 16))
-                .foregroundStyle(AppColor.blueMain)
-                .padding(4)
-            })
-            .padding(6)
-            .background(AppColor.gray00)
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 5))
-            .buttonStyle(.plain)
-            .confirmationDialog(isPresented: $isPresentBedTimePicker) {
-              CustomWheelPicker(selectedIndex: $bedPickerIndex)
-                .background {
-                  RoundedRectangle(cornerRadius: 10)
-                    .fill(AppColor.blueMain.opacity(0.5))
-                    .frame(width: 300, height: 40)
-                }
-            } actions: {
-              SheetAction(title: "ok", role: .default) {
-                isShownBedAlert = true
-              }
-              SheetAction(title: "cancel", role: .cancel)
-            }
-          }
+        }
       }
       
       //            Section("화면 모드") {
@@ -127,53 +125,52 @@ struct SettingView: View {
       
     }
     .navigationTitle("설정")
-    .alert("기상 시간 변경하기",
-           isPresented: $isShownWakeupAlert) {
-      Button(role: .cancel) {
-        
-      } label: {
-        Text("취소")
-      }
-      Button {
-        wakeupTimeIndex = wakeupPickerIndex
-        
-        settingViewModel.effect(.updateSleepTimeButtonTapped(
-          wakeupTimeIndex,
-          bedTimeIndex
-        ))
-        homeViewModel.effect(.updateSleepTimeButtonTapped)
-      } label: {
-        Text("확인")
-      }
-    } message: {
-      let before = wakeupTimeIndex.convertToKoreanTimeFormat()
-      let after = wakeupPickerIndex.convertToKoreanTimeFormat()
-      Text("\(before)에서 \(after)로 변경하시겠습니까?")
-    }
-    .alert("취침 시간 변경하기",
-           isPresented: $isShownBedAlert) {
-      Button(role: .cancel) {
-        
-      } label: {
-        Text("취소")
-      }
-      Button {
-        bedTimeIndex = bedPickerIndex
-        
-        settingViewModel.effect(.updateSleepTimeButtonTapped(
-          wakeupTimeIndex,
-          bedTimeIndex
-        ))
-        homeViewModel.effect(.updateSleepTimeButtonTapped)
-      } label: {
-        Text("확인")
-      }
-    } message: {
-      let before = bedTimeIndex.convertToKoreanTimeFormat()
-      let after = bedPickerIndex.convertToKoreanTimeFormat()
-      Text("\(before)에서 \(after)로 변경하시겠습니까?")
-    }
-
+//    .alert("기상 시간 변경하기",
+//           isPresented: $isShownWakeupAlert) {
+//      Button(role: .cancel) {
+//        
+//      } label: {
+//        Text("취소")
+//      }
+//      Button {
+//        wakeupTimeIndex = wakeupPickerIndex
+//        
+//        settingViewModel.effect(.updateSleepTimeButtonTapped(
+//          wakeupTimeIndex,
+//          bedTimeIndex
+//        ))
+//        homeViewModel.effect(.updateSleepTimeButtonTapped)
+//      } label: {
+//        Text("확인")
+//      }
+//    } message: {
+//      let before = wakeupTimeIndex.convertToKoreanTimeFormat()
+//      let after = wakeupPickerIndex.convertToKoreanTimeFormat()
+//      Text("\(before)에서 \(after)로 변경하시겠습니까?")
+//    }
+//    .alert("취침 시간 변경하기",
+//           isPresented: $isShownBedAlert) {
+//      Button(role: .cancel) {
+//        
+//      } label: {
+//        Text("취소")
+//      }
+//      Button {
+//        bedTimeIndex = bedPickerIndex
+//        
+//        settingViewModel.effect(.updateSleepTimeButtonTapped(
+//          wakeupTimeIndex,
+//          bedTimeIndex
+//        ))
+//        homeViewModel.effect(.updateSleepTimeButtonTapped)
+//      } label: {
+//        Text("확인")
+//      }
+//    } message: {
+//      let after = bedPickerIndex.convertToKoreanTimeFormat()
+//      Text("\(after)로 변경하시겠습니까?")
+//    }
+    
   }
   
 }
